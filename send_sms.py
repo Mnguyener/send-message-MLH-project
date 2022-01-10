732767a4f852a0f79fae07e51975ce8a6d545452
Michelle <52382029+Mnguyener@users.noreply.github.com>
2022-01-10 03:03:52 -0600

    Initial commit

---
+++ send_sms.py
@@ -0,0 +1,22 @@
+from twilio.rest import Client
+from dotenv import load_dotenv
+import os # gives commands to find the env vars
+
+load_dotenv() # loads.env file
+
+# Your Account SID from twilio.com/console
+    # environment variables
+ACCOUNT_SID = os.getenv("ACCOUNT_SID")
+# Your Auth Token from twilio.com/console
+AUTH_TOKEN  = os.getenv("AUTH_TOKEN")
+# created variables
+TO_NUMBER = os.getenv("TO_NUMBER")
+FROM_NUMBER = os.getenv("FROM_NUMBER")
+client = Client(ACCOUNT_SID, AUTH_TOKEN)
+
+message = client.messages.create(
+    to=TO_NUMBER, 
+    from_=FROM_NUMBER,
+    body="This is a test to send a text message from VS")
+
+print(message.sid)
